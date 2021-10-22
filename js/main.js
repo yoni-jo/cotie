6761cc105ae81a544d29871472292f08f9cf7b40
yoni-jo <yoni9258@naver.com>
2021-10-22 16:46:00 +0900

    JS: gnb 메뉴 호버시 숨겨진 lnb 메뉴 토글이벤트

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+//header
+//gnb메뉴 호버시 lnb 메뉴가 보이는 이벤트
+
+const gnb_menu = document.querySelectorAll(".gnb__left>.list")
+const lnb_box_el = document.querySelectorAll(".lnb_box")
+const ON = "on"
+
+gnb_menu.forEach((gnb_menus, index) => {
+  lnb_box_el.forEach((lnb_box, num) => {
+    gnb_menus.addEventListener("mouseenter", () => {
+      lnb_box.classList.remove(ON)
+      if (index === num) {
+        lnb_box_el[num].classList.add(ON)
+        lnb_box_el[num].addEventListener("mouseleave", () => {
+          lnb_box_el[num].classList.remove(ON)
+        })
+      }
+    })
+  })
+})
 // FOOTER
 
 // cotieshop 버튼 클릭시 주소보이는 이벤트
